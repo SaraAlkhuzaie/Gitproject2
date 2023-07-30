c162d0b3672d814a7e979251e616feb6d5e65ba8
saifA <saifA@Saber.myfiosgateway.com>
2023-07-29 20:13:20 -0400

    created new class

---
+++ src/day1/DemoDay1.java
@@ -0,0 +1,12 @@
+package day1;
+
+public class DemoDay1 {
+	public static void main(String[] args) {
+		System.out.println("test");
+	}
+	public static void add(int a, int b) {
+		System.out.println(a+b);
+		
+	}
+
+}
