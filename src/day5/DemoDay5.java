8b1d90674e5c60bc7459cce8271fc56d427b6771
saifA <saifA@Saber.myfiosgateway.com>
2023-07-29 21:40:17 -0400

    new

--- src/day5/DemoDay5.java
+++ src/day5/DemoDay5.java
@@ -4,6 +4,9 @@ public class DemoDay5 {
 	public static void main(String[] args) {
 		System.out.println( "it is a sunny day");
 	}
+	public void getInfo() {
+		System.out.println("Java is good");
+	}
 public void getdata() {
 		System.out.println("igithub");
 	}
