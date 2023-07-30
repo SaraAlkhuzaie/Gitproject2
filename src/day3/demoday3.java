c4ddad46d67303989c3699ad535841f9249d1c52
SaraAlkhuzaie <140735206+SaraAlkhuzaie@users.noreply.github.com>
2023-07-29 20:48:02 -0400

    Update demoday3.java

--- src/day3/demoday3.java
+++ src/day3/demoday3.java
@@ -4,5 +4,8 @@ public class demoday3 {
 	public static void main(String[] args) {
 		System.out.println("my name is sara");
 	}
+	public static void add(int a, int b) {
+		System.out.println(a+b);
+	}
 
 }
