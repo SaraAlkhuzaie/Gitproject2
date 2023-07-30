9ef97ac6bfa77f90e71ef1106a0267bbf80051e8
saifA <saifA@Saber.myfiosgateway.com>
2023-07-29 21:26:58 -0400

    new package

--- src/day1/DemoDay1.java
+++ src/day1/DemoDay1.java
@@ -4,6 +4,9 @@ public class DemoDay1 {
 	public static void main(String[] args) {
 		System.out.println("test");
 	}
+	public void getdata() {
+		System.out.println("Data");
+	}
 	public static void add(int a, int b) {
 		System.out.println(a+b);
 		
